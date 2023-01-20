f5c886098c0ab02e4ed2039318ea5cdc56f422e9
Indrajeet Giri <Indrajeetgiri007@gmail.com>
2023-01-20 13:23:59 +0530

    token added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -8,6 +8,17 @@ const SCOPE = "user_profile,user_media";
 const RESPONSE_TYPE = "code";
 
 const Home = () => {
+	const [token, setToken] = useState("");
+	useEffect(() => {
+		const proxy = new Proxy(new URLSearchParams(window.location.search), {
+			get: (searchParams, param) => searchParams.get(param),
+		});
+		if (proxy.code) {
+			setToken(proxy.code);
+		}
+	}, []);
+
+	console.log(token);
 	const handleAuthorize = () => {
 		window.location = `https://api.instagram.com/oauth/authorize?client_id=${APP_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=${RESPONSE_TYPE}`;
 	};
