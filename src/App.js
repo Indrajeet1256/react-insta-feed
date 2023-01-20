186a9ceaa1962e5a79118735861bb23ed2a34950
Indrajeet Giri <Indrajeetgiri007@gmail.com>
2023-01-20 12:38:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-
 const App = () => {
 	return (
 		<main>
