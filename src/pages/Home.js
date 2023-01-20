ec8f346f8319134941603c1cbde41ef3997b45e7
Indrajeet Giri <Indrajeetgiri007@gmail.com>
2023-01-20 14:00:59 +0530

    token added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const APP_ID = "758057702404913";
 const REDIRECT_URI = "https://hilarious-cuchufli-538de2.netlify.app/";
 const SCOPE = "user_profile,user_media";
 const RESPONSE_TYPE = "code";
+const CLIENT_SECRET = "44eaf13ea1323ad33e8a6c6974d7762c";
 
 const Home = () => {
-	const [token, setToken] = useState("");
+	const [code, setCode] = useState("");
+
 	useEffect(() => {
 		const proxy = new Proxy(new URLSearchParams(window.location.search), {
 			get: (searchParams, param) => searchParams.get(param),
 		});
 		if (proxy.code) {
-			setToken(proxy.code);
+			setCode(proxy.code);
 		}
 	}, []);
 
-	console.log(token);
+	useEffect(() => {
+		if (!code) return;
+		async function getToken() {
+			const options = {
+				client_id: APP_ID,
+				redirect_uri: REDIRECT_URI,
+				grant_type: "authorization_code",
+				code,
+				client_secret: CLIENT_SECRET,
+			};
+			try {
+				const response = await axios.post(
+					"https://api.instagram.com/oauth/access_token",
+					options
+				);
+				console.log(response);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		getToken();
+	}, [code]);
+
 	const handleAuthorize = () => {
 		window.location = `https://api.instagram.com/oauth/authorize?client_id=${APP_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=${RESPONSE_TYPE}`;
 	};
 	return (
 		<section>
-			{token && <Link to="/dashboard">Go To Dashboard</Link>}
+			<Link to="/dashboard">Go To Dashboard</Link>
 			<button type="button" onClick={handleAuthorize}>
 				Authorize With Instagram
 			</button>
